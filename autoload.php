bfc57e829325ce7309a74312125d5b2d58b30baa
zerox <as234zx12@yandex.ru>
2020-10-20 15:46:48 +0300

    фикс

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 namespace DigitalStars\Daemon;
 
-require_once __DIR__."/src/config.php";
+require_once __DIR__."/src/config_daemon.php";
 require_once __DIR__."/src/Server.php";
 require_once __DIR__."/src/Client.php";
